2c48ead7f578748539531e33c7c225313bb409d9
Eso <63209900+esryllmz@users.noreply.github.com>
2022-08-20 18:38:04 +0300

    Add files via upload

---
+++ C#/4 İşlem/4 İşlem/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4_İşlem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int s1, s2, toplam, fark, bölme, carpma,islem;
+
+
+            s1 = Convert.ToInt16(textBox1.Text);
+            s2 = Convert.ToInt16(textBox2.Text);
+            islem = Convert.ToInt16(textBox3.Text);
+
+            toplam = s1+s2;
+            fark = s1 - s2;
+            bölme = s1 / s2;
+            carpma = s1 * s2;
+
+
+            switch (islem)
+            {
+                case:
+                    break;
+            }
+
+
+
+        }
+    }
+}
